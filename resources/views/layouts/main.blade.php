72ae4184656459e5f87aeb49ff0af75d5652480c
‘Crown1004;GIT_AUTHOR_EMAIL=kayevers54324595@outlook.com;GIT_COMMITTER_NAME=‘Crown1004 <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2023-03-19 14:23:58 +0200

    04. بناء القالب الأساسي للموقع

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name') }}</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    {{-- Cairo Font --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo&display=swap" rel="stylesheet">
+
+    <style>
+        body {
+            font-family: 'Cairo', sans-serif;
+            background-color: #f0f0f0
+        }
+    </style>
+
+    @yield('style')
+</head>
+
+<body dir="rtl" style="text-align: right">
+
+    <div>
+        @include('partials.navbar')
+    </div>
+
+    <main class="py-4 md-5">
+        <div class="container">
+            <div class="row">
+                {{-- @include('alerts.success') --}}
+
+                @yield('content')
+            </div>
+        </div>
+
+        @include('partials.footer')
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
+    {{-- Font Awesome Kit --}}
+    <script src="https://kit.fontawesome.com/c49981de61.js" crossorigin="anonymous"></script>
+
+    @yield('script')
+</body>
+
+</html>
