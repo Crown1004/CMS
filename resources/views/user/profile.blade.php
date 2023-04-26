69167b9d5b71dc9af47ed1766e4884fa4dc98603
‘Crown1004;GIT_AUTHOR_EMAIL=kayevers54324595@outlook.com;GIT_COMMITTER_NAME=‘Crown1004 <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2023-04-26 13:57:40 +0200

    02. عرض منشورات المستخدم

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -11,9 +11,15 @@
 
         <div class="row p-3">
             <ul class="nav nav-tabs mb-3">
-                    <li style="list-style:none"><a class="nav-link" href="">posts</a></li>
-                    <li style="list-style:none"><a class="nav-link" href="">comments</a></li>
+                <li class="nav-item" style="list-style:none">
+                    <a class="nav-link" href=""> {{__('منشوراتي')}} </a>
+                </li>
+                <li class="nav-item" style="list-style:none">
+                    <a class="nav-link" href=""> {{__('تعليقاتي')}} </a>
+                </li>
             </ul>
+
+            @include('user.posts_section')
 
         </div>
     </div>
